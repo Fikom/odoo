d3002976918f9fc6273cf2b19e181377b86742d3
FIKOM <103821291+Fikom@users.noreply.github.com>
2022-04-18 21:18:30 +0200

    Update __manifest__.py

--- personal_attendance/__manifest__.py
+++ personal_attendance/__manifest__.py
@@ -14,7 +14,7 @@
     """,
 
     'author': "K. Sander",
-    'website': "-",
+    'website': "https://github.com/Fikom/odoo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
